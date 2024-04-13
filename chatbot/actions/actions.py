de3e75424c551cb5cf5511ac86f49ea110138d19
nishantgott <nishantgk2004@gmail.com>
2024-04-13 22:30:00 +0530

    Added intent and action for listing all courses

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,27 +1,47 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from pymongo import MongoClient
+
+def connect_to_mongodb(mongo_url):
+    try:
+        # Connect to MongoDB
+        client = MongoClient(mongo_url)
+        # Accessing a database
+        dbb = client.get_database()
+        print("Connected to MongoDB successfully!")
+        return dbb
+    except Exception as e:
+        print("Error connecting to MongoDB:", str(e))
+        return None
+
+mongo_url = "mongodb+srv://nishantgk2004:heBmoS6edFGkPTBr@cluster0.jr4xu3z.mongodb.net/ecommerce"
+db = connect_to_mongodb(mongo_url)
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionGiveCourses(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_give_courses"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        try:
+            # Access the "courses" collection in the "ecommerce" database
+            courses_collection = db["courses"]
+            # Retrieve data from the collection
+            courses_data = courses_collection.find()
+            # Print the retrieved data
+            courses_message = "Here are the available courses:\n"
+            for course in courses_data:
+                courses_message += f"{course['course_no']}: {course['course_name']}\n"
+            dispatcher.utter_message(text=courses_message)
+        except Exception as e:
+            print("Error retrieving data from MongoDB:", str(e))
+        dispatcher.utter_message(text="PLEASE CHOOSE THE COURSE NUMBER")
+
+        return []
+
+
