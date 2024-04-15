784b8d25c8c0b88efac018c48fd667acf6f0b28c
nishantgott <nishantgk2004@gmail.com>
2024-04-15 20:17:13 +0530

    added fallback action if the intent confidence is low

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -750,7 +750,15 @@ class ActionExportChatData(Action):
         return []
 
 
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
 
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("I'm sorry, I didn't understand that. Can you please rephrase?")
+        return []
 
 
 
